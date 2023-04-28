e208ccd5f40bccd03482353df14df6e7fe946356
brand421 <57531511+brand421@users.noreply.github.com>
2023-04-28 15:57:27 -0400

    reorganized js for better flow

--- Challenges/Dicee Challenge/index.js
+++ Challenges/Dicee Challenge/index.js
@@ -1,16 +1,15 @@
-var randomNumber1 = Math.round(Math.random()*5)+1;
-var randomNumber2 = Math.round(Math.random()*5)+1;
+var randomNumber1 = Math.floor(Math.random()*6)+1;
 var imageSelector1 = "images/dice" + randomNumber1 + ".png";
-var imageSelector2 = "images/dice" + randomNumber2 + ".png";
-
 document.querySelector(".img1").setAttribute("src", imageSelector1);
 
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var imageSelector2 = "images/dice" + randomNumber2 + ".png";
 document.querySelector(".img2").setAttribute("src", imageSelector2);
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerText = "Player 1 Wins!";
+    document.querySelector("h1").innerText = "🚩Player 1 Wins!";
 } else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerText = "Player 2 Wins!";
+    document.querySelector("h1").innerText = "Player 2 Wins!🚩";
 } else {
     document.querySelector("h1").innerText = "It's a draw!";
 }

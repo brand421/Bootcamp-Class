eda10852880e6f973fa6908c426440e1ca3b1cf2
brand421 <57531511+brand421@users.noreply.github.com>
2023-05-01 16:28:45 -0400

    added keydown event listener

--- Challenges/Drum Kit/index.js
+++ Challenges/Drum Kit/index.js
@@ -2,8 +2,50 @@
 //adds event listeners to all buttons
 var buttons = document.querySelectorAll(".drum");
 
+
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function() {
-        alert("I was clicked!");
+    buttons[i].addEventListener("keydown", function() {
+        makeSound(this.innerHTML);
     })
+}
+
+
+//Detects keyboard press
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+})
+
+function makeSound(key) {
+    switch(key) {
+            case "w":
+                var kick = new Audio('sounds/kick-bass.mp3');
+                kick.play();
+                break;
+            case "a":
+                var snare = new Audio('sounds/snare.mp3');
+                snare.play();
+                break;
+            case "s":
+                var crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                break;
+            case "d":
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+                break;
+            case "j":
+                var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+                break;
+            case "k":
+                var tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+                break;
+            case "l":
+                var tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+                break;
+            default:
+                console.log(this.innerHTML);
+        }
 }

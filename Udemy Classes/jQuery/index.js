7d6cb5b47ea8259e1619a185b815185b7bbc401b
brand421 <57531511+brand421@users.noreply.github.com>
2023-05-02 16:30:08 -0400

    added more jQuery function examples

--- Udemy Classes/jQuery/index.js
+++ Udemy Classes/jQuery/index.js
@@ -1,6 +1,34 @@
 
-$("h1").text("bye");
+// $("h1").text("bye");
 
 // $("button").text("Don't click");
 
-$("button").html("<em>Hello!<em>")
+// $("button").html("<em>Hello!<em>")
+
+// console.log($("img").attr("src"));
+
+// $("a").attr("href", "http://www.yahoo.com");
+
+$("h1").click(function() {
+    $("h1").css("color", "green");
+});
+
+// $("button").click(function() {
+    // $("h1").hide();
+    // $("h1").toggle();
+    // $("h1").fadeOut();
+    // $("h1").fadeToggle();
+    // $("h1").slideToggle();
+// });
+
+$(document).keydown(function (e) { 
+    $("h1").text(e.key);
+});
+
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "green");
+})
+
+$("button").on("click", function() {
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
+})

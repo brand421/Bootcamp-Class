1f362cc8c40f04cd184b32d2e072bf096fe0bb70
brand421 <57531511+brand421@users.noreply.github.com>
2023-05-03 14:20:26 -0400

    added level tracking

--- Challenges/Simon Game/game.js
+++ Challenges/Simon Game/game.js
@@ -2,25 +2,40 @@
 var buttonColors = ["red", "green", "blue", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+
+$(document).keydown(function() {
+    $("h1").text("Level " + level);
+    nextSequence();
+})
 
 $(".btn").click(function() {
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
 })
 
 function nextSequence() {
-    //randomly selects color and pushes to gamePattern
+
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
 
-    //animates chosen color
     $("#"+ randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);  
-
-    //plays audio to match selected color 
+    level += 1;
+    $("h1").text("Level " + level);
 }
 
 function playSound(name) {
     var gameAudio = new Audio("sounds/" + name + ".mp3");
     gameAudio.play();
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColor).removeClass("pressed")
+    }, 100);
 }

816043f35a22a57b21b0d76f7ffad85f19896e93
brand421 <r3v9191@gmail.com>
2023-05-04 20:16:10 -0400

    created basic get commands

--- Udemy Classes/my-express-server/server.js
+++ Udemy Classes/my-express-server/server.js
@@ -1,2 +1,18 @@
 const express = require("express");
+const app = express();
 
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
+
+app.get("/", function(req, res) {
+    res.send("<h1>Hello world!</h1>");
+});
+
+app.get("/contact", function(req, res) {
+    res.send("Contact me at: g@gmail.com");
+})
+
+app.get("/about", function(req, res) {
+    res.send("I'm brandan, and this is my about page that's pretty empty!");
+});
